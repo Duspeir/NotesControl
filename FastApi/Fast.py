067f24075292fe5dd855294cfca30adf80b18816
Dusp <vovarus353@gmail.com>
2024-10-11 21:46:04 +0600

    creating back-"server"

--- FastApi/Fast.py
+++ FastApi/Fast.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI
 import uvicorn
+from starlette.middleware.cors import CORSMiddleware
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def hello():
@@ -9,8 +23,6 @@ def hello():
 @app.get("/fast")
 def fast():
     return "Fast"
-
-
 
 if __name__ == "__main__":
     uvicorn.run("Fast:app", reload=True)
